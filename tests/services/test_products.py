000c839300c328abf405edc98161a1400c4467cd
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2024-02-10 19:46:33 +0200

    Add all products test

--- tests/services/test_products.py
+++ tests/services/test_products.py
@@ -4,6 +4,7 @@ from core.api.v1.products.filters import ProductFilters
 from tests.factories.products import ProductModelFactory
 from tests.services.conftest import product_service
 from core.apps.products.services.products import IProductService
+from core.api.filters import PaginationIn
 
 
 @pytest.mark.django_db
@@ -20,5 +21,13 @@ def test_get_products_count_exist(product_service: IProductService):
     assert products_count == 0
 
 @pytest.mark.django_db
-def test_product_search():
-    assert False
+def test_get_products_all():
+    expected_count = 5
+    products = ProductModelFactory.create_batch(size=expected_count)
+    products_titles = {product.title for product in products}
+
+    fetched_products = product_service.get_product_list(ProductFilters(), PaginationIn())
+    fetched_titles = {product.title for product in fetched_products}
+
+    assert len(fetched_products) == expected_count
+    assert products_titles == fetched_titles
