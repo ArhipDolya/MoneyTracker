138691ad592054b005d8f6f03a4574b3b59e145c
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2024-02-10 19:34:40 +0200

    Add some tests

--- tests/services/conftest.py
+++ tests/services/conftest.py
@@ -1,5 +1,8 @@
+import pytest
+
 from core.apps.products.services.products import IProductService, ORMProductService
 
 
+@pytest.fixture
 def product_service() -> IProductService:
     return ORMProductService()
--- tests/services/test_products.py
+++ tests/services/test_products.py
@@ -1,12 +1,23 @@
 import pytest
 
+from core.api.v1.products.filters import ProductFilters
+from tests.factories.products import ProductModelFactory
 from tests.services.conftest import product_service
 from core.apps.products.services.products import IProductService
 
 
 @pytest.mark.django_db
-def test_products_count(product_service: IProductService):
-    assert True
+def test_get_products_count_zero(product_service: IProductService):
+    expected_count = 5
+    ProductModelFactory.create_batch(size=expected_count)
+
+    products_count = product_service.get_product_count(ProductFilters())
+    assert products_count == expected_count
+
+@pytest.mark.django_db
+def test_get_products_count_exist(product_service: IProductService):
+    products_count = product_service.get_product_count(ProductFilters())
+    assert products_count == 0
 
 @pytest.mark.django_db
 def test_product_search():
