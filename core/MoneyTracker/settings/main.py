f95d74d6653e47a99f5a2499dbd5f25f6bcd815a
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2024-02-11 15:31:18 +0200

    Add model for customers

--- core/MoneyTracker/settings/main.py
+++ core/MoneyTracker/settings/main.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
 
     # First party
     'core.apps.products',
+    'core.apps.customers',
 ]
 
 MIDDLEWARE = [
--- core/apps/customers/models.py
+++ core/apps/customers/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from core.apps.common.models import TimedBaseModel
 
-# Create your models here.
+
+class Customer(TimedBaseModel):
+    phone = models.CharField(verbose_name='Phone Number', max_length=16, unique=True)
+    token = models.CharField(verbose_name='User Token', max_length=255, unique=True)
+
+    def __str__(self):
+        return self.phone
+    
+    def __repr__(self):
+        return self.phone
+
+    class Meta:
+        verbose_name = 'Customer'
+        verbose_name_plural = 'Customers'
