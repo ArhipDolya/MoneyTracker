640c7b38e59679aa4dc5f4d92820a97d4ff7afa3
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2024-02-11 17:33:31 +0200

    Add dummy service for senders

--- core/apps/customers/services/senders.py
+++ core/apps/customers/services/senders.py
@@ -1,8 +1,15 @@
 from abc import ABC, abstractmethod
 
+from core.apps.customers.entities import CustomerEntity
+
 
 class BaseSenderService(ABC):
     
     @abstractmethod
-    def send_code(self, code: str) -> str:
+    def send_code(self, customer: CustomerEntity, code: str) -> None:
         ...
+
+
+class DummySenderService(BaseSenderService):
+    def send_code(self, customer: CustomerEntity, code: str) -> None:
+        print(f'Code sent to user: {customer}, code: {code}')
